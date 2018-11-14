42eefd7bb30da07528530685b4a278412f96c682
Howard Cheung <howardcheung@USF-Gold-Wifi-226-21-184.laptops.usf.edu>
2018-11-14 17:40:38 -0500

    Found a way to reference from the same level and static issue for now

---
+++ Analysis of Algorithms/Final Project/JumpingJim/JumpingJim.java
@@ -0,0 +1,18 @@
+//import JumpingJim.*;
+
+public class JumpingJim
+{
+    public static void main(String args[])
+    {
+        for (int i = 0; i < 5; i++)
+        {    
+            Node node = new Node();
+        }
+        BFS();
+    }
+
+    public static void BFS()
+    {
+        Node node = new Node();
+    }
+}
