79c0be46802df023d677291e50c7e6e1d1354b46
Howard Cheung <howardcheung@USF-Gold-Wifi-226-21-184.laptops.usf.edu>
2018-11-14 18:07:44 -0500

    Edited Node class to match what I had. Need to add gitignore for .vscode extension

--- Analysis of Algorithms/Final Project/JumpingJim/Node.java
+++ Analysis of Algorithms/Final Project/JumpingJim/Node.java
@@ -1,7 +1,27 @@
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Node
 {
-    Node() {}
+    private Node previous;
+    private Node next;
+    private int number; 
+
+    Node() { this.previous = this.next = null; this.number = -1; }
+    Node (Node previous, Node next, int number)
+    {
+        this.previous = previous;
+        this.next = next;
+        this.number = number;
+    }
+
+    public Node getPrevious() { return this.previous; }
+    public void setPrevious(Node previous) { this.previous = previous; }
+
+    public Node getNext() { return this.next; }
+    public void setNext(Node next) { this.next = next; }
+
+    public int getNumber() { return this.number; }
+    public void setNumber(int number) { this.number = number; }
 }
