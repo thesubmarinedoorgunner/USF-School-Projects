78a5af4f457fcec4d685768bde6c87b0ce24918a
the_submarine_doorgunner <cheung@mail.usf.edu>
2018-11-24 19:07:30 -0500

    Got the input file to work

--- Analysis of Algorithms/Final Project/JumpingJim/JumpingJim.java
+++ Analysis of Algorithms/Final Project/JumpingJim/JumpingJim.java
@@ -1,18 +1,64 @@
 //import JumpingJim.*;
 
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class JumpingJim
 {
     public static void main(String args[])
     {
-        for (int i = 0; i < 5; i++)
-        {    
-            Node node = new Node();
+        int row, column;
+        int[][] matrix;
+        File file = new File("input.txt");
+        // if (file != null)
+        // {
+        //     System.out.println("File read?");
+        // }
+        try 
+        {
+            Scanner s = new Scanner(file);
+            row = s.nextInt();
+            column = s.nextInt();
+            matrix = new int[row][column];
+
+            int i = 0, j = 0;
+
+            while ( s.hasNextLine() )
+            {
+                int a = s.nextInt();
+                matrix[i][j] = a;
+                j++;
+                if ( j == column ) { j = 0; i++; }
+                if ( i == row ) { break; }
+            }
+            s.close();
+
+            for (int x = 0; x < row; x++)
+            {
+                for (int y = 0; y < column; y++)
+                {
+                    System.out.print(matrix[x][y] + " ");
+                }
+                System.out.println();
+            }
+        } 
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("Input file is corrupted or incorrect");
         }
-        BFS();
+
+
+
+        // for (int i = 0; i < 5; i++)
+        // {    
+        //     Node node = new Node();
+        // }
+        // BFS();
     }
 
-    public static void BFS()
-    {
-        Node node = new Node();
-    }
+    // public static void BFS()
+    // {
+    //     Node node = new Node();
+    // }
 }
